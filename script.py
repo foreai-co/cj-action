d1cf891fdb8b68fbe26c07c71791d997150c6b19
Grigory Khromov <grigory@foreai.co>
2024-12-12 17:22:59 +0100

    feat: implement service account auth

--- script.py
+++ script.py
@@ -1,13 +1,22 @@
 import os
 import sys
 import time
+
 import requests
 
 # Get input from environment variables
 # These ENV vars are set by github actions based on action.yml
 test_id = os.getenv("INPUT_TEST_ID", "")
-cj_token = os.getenv("INPUT_TOKEN", "")
+service_account_key = os.getenv("INPUT_SERVICE_ACCOUNT_KEY", "")
 MAX_FETCHES = 10
+POLL_EVERY_SECONDS = 10.0
+BACKEND_URL = "https://cj-backend.foreai.co"
+
+def _get_headers(token: str) -> dict:
+    return {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
+    }
 
 def run() -> tuple[bool, str]:
     """Business logic for the action.
@@ -17,17 +26,19 @@ def run() -> tuple[bool, str]:
             - The first element says whether the test run was successful or not.
             - The second element is a message shown in the Github output.
     """
-    if not (test_id and cj_token):
+    if not (test_id and service_account_key):
         return False, "Failed: All required fields were not provided."
 
-    headers = {
-        "Authorization": f"Bearer {cj_token}",
-        "Content-Type": "application/json"
-    }
+    # Login the serive account
+    response = requests.post(f"{BACKEND_URL}/auth/login_service_account",
+                             headers=_get_headers(service_account_key))
+    if response.status_code != 200:
+        return False, f"Failed to login service account: {response.json()}"
 
-    test_run_ep_stub = "https://cj-backend.foreai.co/test-run"
+    jwt_token = response.text
 
-    response = requests.post(f"{test_run_ep_stub}/{test_id}",
+    headers = _get_headers(jwt_token)    
+    response = requests.post(f"{BACKEND_URL}/test-run/{test_id}",
                              headers=headers)
 
     if response.status_code != 201:
@@ -37,16 +48,16 @@ def run() -> tuple[bool, str]:
     test_run_id = response.json()
 
     for _ in range(MAX_FETCHES):
-        run_status_response = requests.get(f"{test_run_ep_stub}/{test_run_id}",
-                                           headers=headers)
+        run_status_response = requests.get(
+            f"{BACKEND_URL}/test-run/{test_run_id}", headers=headers)
         run_status_response_json = run_status_response.json()
         status = run_status_response_json["status"]
         if status == "passed":
             return True, "Test passed!"
         if status == "failed":
             return False, run_status_response_json["error_message"]
-        # Wait a few secs.
-        time.sleep(10.0)
+
+        time.sleep(POLL_EVERY_SECONDS)
     return False, "Timed out waiting for test result!"
 
 
